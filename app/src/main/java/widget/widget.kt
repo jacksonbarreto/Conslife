eb48061abb4ecd63edfaacab2de446bd51184c94
Valdo Mpinga <valdompinga57@gmail.com>
2022-04-24 12:54:55 +0100

    Updated setup

---
+++ app/src/main/java/widget/widget.kt
@@ -0,0 +1,12 @@
+package widget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.conslife.R
+
+class widget : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_widget)
+    }
+}
