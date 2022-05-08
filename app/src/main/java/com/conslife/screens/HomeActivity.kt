5e0c622164ea4f5d8ee232a37740e329ba36e315
Jackson Barreto <jackson.barreto@gmail.com>
2022-05-08 11:56:32 +0100

    start

---
+++ app/src/main/java/com/conslife/screens/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.conslife.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.conslife.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
